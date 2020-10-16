13568540b02452405c4d38824e5e58740111a1c9
anuragjoshi3519 <anurag_joshi@outlook.com>
2020-10-17 01:06:17 +0530

    Auth privider class added

---
+++ lib/providers/auth.dart
@@ -0,0 +1,74 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_config/flutter_config.dart';
+import 'package:http/http.dart' as http;
+
+import '../models/http_exception.dart';
+
+class Auth with ChangeNotifier {
+  String _authToken;
+  String _userId;
+  String _expiryIn;
+  bool _isAuthenticated = false;
+
+  bool get isAuthenticated {
+    return _isAuthenticated;
+  }
+
+  String get userId {
+    return _userId;
+  }
+
+  String get token {
+    return _authToken;
+  }
+
+  void logout(){
+    _authToken="";
+    _userId="";
+    _expiryIn="";
+    _isAuthenticated=false;
+    notifyListeners();
+  }
+  
+  Future<void> authenticate(
+      String email, String password, String action) async {
+    dynamic response;
+    if (action == 'signup') {
+      try {
+        response = await http.post(
+            'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${FlutterConfig.get('API_KEY')}',
+            body: json.encode({
+              'email': email,
+              'password': password,
+              'returnSecureToken': true
+            }));
+      } catch (err) {
+        rethrow;
+      }
+    } else {
+      try {
+        response = await http.post(
+            'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${FlutterConfig.get('API_KEY')}',
+            body: json.encode({
+              'email': email,
+              'password': password,
+              'returnSecureToken': true
+            }));
+      } catch (err) {
+        rethrow;
+      }
+    }
+    response = json.decode(response.body);
+    if (response['error'] != null) {
+      throw HTTPException(response['error']['message']);
+    } else {
+      _authToken = response['idToken'];
+      _userId = response['localId'];
+      _expiryIn = response['expiresIn'];
+      _isAuthenticated = true;
+      notifyListeners();
+    }
+  }
+}
